f4b58df5dc102dd35f82988228716877a422ee12
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-06-18 19:08:57 +0800

    created the react router SignUp and Chat

---
+++ client/src/components/contactsPane/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from "./contactsPane.module.css";
+
+function ContactsPane() {
+    return (
+        <div></div>
+    )
+}
+
+export default ContactsPane;
---
+++ client/src/components/messagesPane/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './messagesPane.module.css'
+
+function MessagePane() {
+    return (
+        <div></div>
+    )
+}
+
+export default MessagePane;
