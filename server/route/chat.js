73bace1ddc0a3bc4ec84976b52b6f6907c2b756e
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-07-23 14:13:59 +0800

    Feature: Add the getOneConversation path

--- server/route/chat.js
+++ server/route/chat.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const chatController = require('../controller/chat');
 const checkAuth = require('../middleware/check-auth');
+const { route } = require('./user');
 
 router.post('/search', chatController.searchUsers);
 router.get('/conversations', checkAuth, chatController.getConversations);
+router.get('/conversations/:conversationId', checkAuth, chatController.getOneConversation);
 router.post('/conversations', checkAuth, chatController.createConversation);
 router.get('/conversations/:conversationId/messages', checkAuth, chatController.getMessages);
 router.post('/conversations/:conversationId/messages', checkAuth, chatController.sendMessage);
