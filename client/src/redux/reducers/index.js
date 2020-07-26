c0e17a474dfd1fbed862fc7629c41237212149dc
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-07-26 16:52:47 +0800

    add currConv to rootReducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 
 import messages from './messages';
+import currConv from './currConv';
 
 export default combineReducers({
-    messages
+    messages,
+    currConv
 })
