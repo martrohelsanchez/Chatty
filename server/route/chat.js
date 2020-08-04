2cd4c639d6ecc6c95327da0d22ccd7b7759716c4
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-04 18:23:51 +0800

    add updateSeen route

--- server/route/chat.js
+++ server/route/chat.js
@@ -7,6 +7,7 @@ const { route } = require('./user');
 router.post('/search', chatController.searchUsers);
 router.get('/conversations', checkAuth, chatController.getConversations);
 router.get('/conversations/:conversationId', checkAuth, chatController.getOneConversation);
+router.patch('/conversations/:conversationId/seen', checkAuth, chatController.updateSeen);
 router.post('/conversations', checkAuth, chatController.createConversation);
 router.get('/conversations/:conversationId/messages', checkAuth, chatController.getMessages);
 router.post('/conversations/:conversationId/messages', checkAuth, chatController.sendMessage);
