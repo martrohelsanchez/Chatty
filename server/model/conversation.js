37464ab0e2193e7fc03817b8b0174ff1c4c7026a
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-04 18:24:51 +0800

    Feature: Add members_meta to conversation model

--- server/model/conversation.js
+++ server/model/conversation.js
@@ -2,18 +2,27 @@ const {Schema, model} = require('mongoose');
 
 const conversationSchema = new Schema({
     _id: {type: Schema.Types.ObjectId},
-    conversation_name: {type: String},
+    group_name: {type: String},
+    is_group_chat: {type: Boolean},
     members: [
         {type: Schema.Types.ObjectId, ref: 'User'}
     ],
     conversation_pic: {type: String},
     last_message: {
         message_body: {type: String},
-        sender_username: {type: String}
+        sender_username: {type: String},
+        date_sent: {type: Number}
     },
     is_chatroom: {type: Boolean},
     created_at: {type: Number},
-    last_updated: {type: Number}
+    last_updated: {type: Number},
+    members_meta: [
+        {
+            user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+            last_seen: {type: Number},
+            delivered: {type: Boolean}
+        }
+    ]
 });
 
 module.exports = model('Conversation', conversationSchema); 
