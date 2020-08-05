cfba6d757eb7e99ad4a56d19719ec45157e46669
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-05 18:43:16 +0800

    Refractor: use redux

--- client/src/components/conversationList/ConversationList.js
+++ client/src/components/conversationList/ConversationList.js
@@ -6,50 +6,19 @@ import Conversation from './conversation/Conversation';
 import Loading from '../loading/Loading';
 import {socket} from '../../App';
 
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {setCurrConv} from '../../redux/actions/currConvActions';
+import {retrieveConversations} from '../../redux/actions/conversationsActions';
 
 function ConversationList() {
-    const [conversations, setConversations] = useState([]);
+    const conversations = useSelector(state => state.conversations);
+    const state = useSelector(state => state);
     const [err, setErr] = useState(null);
     const [isInitialRender, setIsInitialRender] = useState(true);
     const dispatch = useDispatch();
 
     useEffect(() => {
         getConversations();
-
-        socket.on('sendMsg', newMsg =>{
-            for (let i in conversations) {
-                const convId = conversations[i]._id;
-
-                //finds the conversation of the new msg on the current loaded conversation list
-                if (convId === newMsg.conversation_id) {
-                    const updateConversations = [...conversations];
-                    const {last_message, last_updated} = updateConversations[convId];
-
-                    last_message.message_body = newMsg.message_body
-                    last_message.sender_username = newMsg.sender_username
-                    last_updated = newMsg.date_sent;
-
-                    //places the updated conversation to the top
-                    updateConversations.unshift(updateConversations[convId]);
-                    updateConversations.splice(i, 1);
-
-                    setConversations(updateConversations);
-                }
-
-                //finds the conversation of the new msg on the database
-                axios.get('/chat/conversations')
-                    .then(res => {
-                        const newConversation = res.data;
-                        setConversations([newConversation, ...conversations])
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                        setErr('Something went wrong')
-                    })
-            }
-        })
     }, []);
 
     async function getConversations() {
@@ -61,12 +30,11 @@ function ConversationList() {
                 }
             })
 
-            console.log(data)
-
             if (isInitialRender) {
                 dispatch(setCurrConv(data.conversations[0]))
             }
-            setConversations([...conversations, ...data.conversations])
+            dispatch(retrieveConversations(data.conversations))
+            // setConversations([...conversations, ...data.conversations])
         } catch (err) {
             console.error(err.message)
             setErr('Something went wrong')
@@ -78,6 +46,8 @@ function ConversationList() {
     } else if (err) {
         return <div>{err}</div>
     }
+
+    console.log('conversations: ', conversations)
 
     const renderConversations = conversations.map(conv => {
         return <Conversation key={conv._id} conv={conv} />
