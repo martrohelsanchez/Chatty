3cc2cbea515d939731feff0e63b17e6c0f5b81c7
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-05 18:41:31 +0800

    Refractor: Move the socket listener for sendMsg to Chat Comp

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,14 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import {useHistory} from 'react-router-dom';
 
 import ContactsPane from '../contactsPane/ContactsPane';
 import MessagePane from '../messagesPane/messagesPane';
-import {UserInfoContext} from '../../App';
+import {UserInfoContext, socket} from '../../App';
+
+import { useDispatch, useSelector } from 'react-redux';
+import {addNewMsg, updateLastSeen} from '../../redux/actions/conversationsActions';
+
+let currConvId;
 
 function Chat() {
+    currConvId = useSelector(state => state.currConv._id);
     const userInfo = useContext(UserInfoContext);
     const history = useHistory();
-    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        socket.on('sendMsg', newMsg => {
+            //dispatch if the newMsg is for the current conversation
+            console.log('received a message')
+            if (currConvId === newMsg.conversation_id) {
+                dispatch(addNewMsg(currConvId, newMsg));
+            }
+        });
+
+        // socket.on('seen', (convId, seenMeta) => {
+        //     dispatch(updateLastSeen(convId, seenMeta));
+        // });
+    }, [])
+
     if (!userInfo) {
         history.push('/logIn');
         //returning null to prevent returning the ContactsPane and MessagePane components
