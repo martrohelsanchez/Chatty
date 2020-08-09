cd0cc2ce6941f7c3209e0c0b0731bfa17a7bf978
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-09 18:30:59 +0800

    Feature: See who has seen the msg and additional status abt the sent msg

--- client/src/components/msgStatus/MsgStatus.js
+++ client/src/components/msgStatus/MsgStatus.js
@@ -1,38 +1,73 @@
 import React, { useContext } from 'react';
 
 import { UserInfoContext } from '../../App';
+import styles from './msgStatus.module.css';
 
 import delivered from '../../images/delivered.svg';
 import sending from '../../images/sending.svg';
-import set from '../../images/sent.svg';
+import sent from '../../images/sent.svg';
 import seen from '../../images/seen.svg';
 
-function MsgStatus({allMsg, msgIndex, membersMeta, currMsg}) {
+function MsgStatus({allMsg, msgIndex, membersMeta, currMsg, isDelivered}) {
     const user = useContext(UserInfoContext);
-    const nextMsg = allMsg[Number(msgIndex) + 1];
+    
+    if (allMsg === undefined) return null;
+    
+    const nextMsg = allMsg[Number(msgIndex) + 1] || {};
+    const isLastMsg = allMsg.length - 1 === msgIndex;
     let lastSeenMembers = [];
     
     for (let member of membersMeta) {
-        if (member.last_seen >= currMsg.date_sent && member.last_seen <= currMsg.date_sent) {
-            lastSeenMembers.push(member.user_id)
+        const notFromUser = user.userId !== currMsg.sender._id;
+        const afterCurrMsg = member.last_seen >= currMsg.date_sent;
+        const beforeNextMsg = member.last_seen < nextMsg.date_sent;
+        const userLastSeenMsg = isLastMsg ? afterCurrMsg : afterCurrMsg && beforeNextMsg;
+
+        if (userLastSeenMsg && notFromUser) {
+            lastSeenMembers.push(member.user_id);
         }
     }
 
-    let seenHeads;
+    let seenHeads = [];
 
     if (lastSeenMembers.length > 0) {
         seenHeads = lastSeenMembers.map(member => {
             return (
-                <img src={seen} />
+                <img key={member} className={styles.status} src={seen} />
             )
         })
+        return (
+            <div>
+                {seenHeads}
+            </div>
+        )
     }
 
-    return (
-        <div>
+    if (user.userId === currMsg.sender._id && isLastMsg) {
+        const isSent = currMsg.is_sent === undefined ? true : currMsg.is_sent;
 
-        </div>
-    )
+        if (!isSent) {
+            return (
+                <div>
+                    <img className={styles.status} src={sending} />
+                </div>
+            )
+        } else if (isSent && !isDelivered) {
+            return (
+                <div>
+                    <img className={styles.status} src={sent} />
+                </div>
+            )
+        } else if (isDelivered) {
+            return (
+                <div>
+                    <img className={styles.status} src={delivered} />
+                </div>
+            )
+        }
+    }
+
+    return null;
 }
 
 export default MsgStatus;
