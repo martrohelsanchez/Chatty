3ac7ebbbe028944765726d581b94eb979f6894af
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-09 18:30:01 +0800

    delete isSent state getters and setters

--- client/src/components/messagesPane/messagesPane.js
+++ client/src/components/messagesPane/messagesPane.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './messagesPane.module.css'
 import InputBar from '../inputBar/InputBar'
 import MessageList from './messages/MessageList'
 
 function MessagePane() {
+    const [isDelivered, setIsDelivered] = useState(false);
+    
     return (
         <div className={styles.messagePaneContainer}>
-            <MessageList />
+            <MessageList isDelivered={isDelivered} />
             <InputBar />
         </div>
     )
