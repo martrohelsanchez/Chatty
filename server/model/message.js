8d37f3844b364be33845ef43d26615168e75f57b
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-22 12:27:44 +0800

    ♻️Rearranged fields

--- server/model/message.js
+++ server/model/message.js
@@ -2,10 +2,10 @@ const {Schema, model} = require('mongoose');
 
 const messageSchema = new Schema({
     _id: {type: Schema.Types.ObjectId},
-    conversation_id: {type: Schema.Types.ObjectId, ref: 'Conversation'},
     sender: {type: Schema.Types.ObjectId, ref: 'User'},
     message_body: {type: String},
-    date_sent: {type: Number}
+    date_sent: {type: Number},
+    conversation_id: {type: Schema.Types.ObjectId, ref: 'Conversation'}
 });
 
 module.exports = model('Message', messageSchema);
