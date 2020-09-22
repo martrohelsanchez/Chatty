6d1495fcda2f06c4fa840a68028814bbd24c1bf1
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-22 21:18:43 +0800

    🔨Corrected fields

--- server/src/model/membersMeta.ts
+++ server/src/model/membersMeta.ts
@@ -2,14 +2,20 @@ import {Schema, model, Document} from 'mongoose';
 
 interface IMembersMetaSchema extends Document {
     conversation_id: string;
-    user_id: string;
-    last_seen: string;
+    members_meta: {
+        user_id: string;
+        last_seen: number;
+    }[];
 }
 
 const membersMetaSchema = new Schema({
-    conversation_id: {type: Schema.Types.ObjectId, ref: 'Conversation'},
-    user_id: {tyjpe: Schema.Types.ObjectId, ref: 'User'},
-    last_seen: {type: Number},
+    conversation_id: {type: Schema.Types.ObjectId},
+    members_meta: [
+        {
+            user_id: {tyjpe: Schema.Types.ObjectId},
+            last_seen: {type: Number}
+        }
+    ]
 });
 
 export default model<IMembersMetaSchema>('MembersMeta', membersMetaSchema);
