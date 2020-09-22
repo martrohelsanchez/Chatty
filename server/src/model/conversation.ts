7422704ff8bae3857537fb574815332446a9d49c
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-22 16:11:12 +0800

    🏷️🚚Convert to ts and move members_meta to its own collection

---
+++ server/src/model/conversation.ts
@@ -0,0 +1,52 @@
+import {Schema, model, Document} from "mongoose";
+
+// const conversationSchema = new Schema({
+//     _id: {type: Schema.Types.ObjectId},
+//     group_name: {type: String},
+//     is_group_chat: {type: Boolean},
+//     members: [
+//         {type: Schema.Types.ObjectId, ref: 'User'}
+//     ],
+//     conversation_pic: {type: String},
+//     last_message: {
+//         sender_id: {type: Schema.Types.ObjectId},
+//         sender_username: {type: String},
+//         message_body: {type: String},
+//         date_sent: {type: Number},
+//         is_delivered: {type: Boolean}
+//     },
+//     created_at: {type: Number},
+//     last_updated: {type: Number},
+//     members_meta: [
+//         {
+//             user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+//             last_seen: {type: Number},
+//             delivered: {type: Boolean}
+//         }
+//     ]
+// });
+
+interface IConversationSchema extends Document {
+  group_name: string;
+  is_group_chat?: string;
+  members: string[];
+  conversation_pic: string;
+  last_message: string;
+  created_at: number;
+  last_updated: number;
+  members_meta: string;
+} 
+
+const conversationSchema = new Schema({
+  _id: {type: Schema.Types.ObjectId},
+  group_name: {type: String},
+  is_group_chat: {type: Boolean},
+  members: [{type: Schema.Types.ObjectId, ref: "User"}],
+  conversation_pic: {type: String},
+  last_message: {type: Schema.Types.ObjectId, ref: "Message"},
+  created_at: {type: Number},
+  last_updated: {type: Number},
+  members_meta: {type: Schema.Types.ObjectId, ref: 'MembersMeta'}
+});
+
+module.exports = model<IConversationSchema>('Conversation', conversationSchema); 
