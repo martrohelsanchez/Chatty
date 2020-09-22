1fb561608eec8efa1ee934808956ebbb3a1911ab
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-22 11:48:39 +0800

    🏷️Convert last_message to type of objectId which is a reference for Message

--- server/model/conversation.js
+++ server/model/conversation.js
@@ -1,29 +1,47 @@
 const {Schema, model} = require('mongoose');
 
+// const conversationSchema = new Schema({
+//     _id: {type: Schema.Types.ObjectId},
+//     group_name: {type: String},
+//     is_group_chat: {type: Boolean},
+//     members: [
+//         {type: Schema.Types.ObjectId, ref: 'User'}
+//     ],
+//     conversation_pic: {type: String},
+//     last_message: {
+//         sender_id: {type: Schema.Types.ObjectId},
+//         sender_username: {type: String},
+//         message_body: {type: String},
+//         date_sent: {type: Number},
+//         is_delivered: {type: Boolean}
+//     },
+//     created_at: {type: Number},
+//     last_updated: {type: Number},
+//     members_meta: [
+//         {
+//             user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+//             last_seen: {type: Number},
+//             delivered: {type: Boolean}
+//         }
+//     ]
+// });
+
 const conversationSchema = new Schema({
-    _id: {type: Schema.Types.ObjectId},
-    group_name: {type: String},
-    is_group_chat: {type: Boolean},
-    members: [
-        {type: Schema.Types.ObjectId, ref: 'User'}
-    ],
-    conversation_pic: {type: String},
-    last_message: {
-        message_body: {type: String},
-        sender_username: {type: String},
-        date_sent: {type: Number},
-        sender_id: {type: Schema.Types.ObjectId},
-        is_delivered: {type: Boolean}
+  _id: { type: Schema.Types.ObjectId },
+  group_name: { type: String },
+  is_group_chat: { type: Boolean },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  conversation_pic: { type: String },
+  last_message: {type: Schema.Types.ObjectId, ref: "Message"},
+  created_at: { type: Number },
+  last_updated: { type: Number },
+  members_meta: [
+    {
+      user_id: { type: Schema.Types.ObjectId, ref: "User" },
+      last_seen: { type: Number },
+      delivered: { type: Boolean },
     },
-    created_at: {type: Number},
-    last_updated: {type: Number},
-    members_meta: [
-        {
-            user_id: {type: Schema.Types.ObjectId, ref: 'User'},
-            last_seen: {type: Number},
-            delivered: {type: Boolean}
-        }
-    ]
+  ],
 });
 
 module.exports = model('Conversation', conversationSchema); 
