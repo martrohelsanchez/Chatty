6ca2c996ab60ebb932ec08692a384bbbd2d168eb
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-25 14:03:16 +0800

    🔒Don't check csrf token if env is development

--- server/src/middleware/check-auth.ts
+++ server/src/middleware/check-auth.ts
@@ -12,18 +12,20 @@ module.exports = (
   next: NextFunction
 ) => {
   try {
+
     const jwtToken = req.cookies.jwt;
     const decoded = jwt.verify(jwtToken, process.env.JWT_KEY) as IJwtDecoded;
     const csrfToken = req.headers["csrf-token"] as string;
 
-    //Verify if the csrf token was generated from the server
-    if (!csrfTokenGen.verify(process.env.CSRF_TOKEN_KEY, csrfToken)) {
-      throw new Error();
-    }
-
-    //The csrf token from req header must be the same from the token inside the jwt
-    if (decoded.csrfToken !== csrfToken) {
-      throw new Error();
+    if (process.env.NODE_ENV === 'production') {
+      //Verify if the csrf token was generated from the server
+      if (!csrfTokenGen.verify(process.env.CSRF_TOKEN_KEY, csrfToken)) {
+        throw new Error();
+      }
+      //The csrf token from req header must be the same from the token inside the jwt
+      if (decoded.csrfToken !== csrfToken) {
+        throw new Error();
+      }
     }
 
     req.decodedJwt = decoded;
