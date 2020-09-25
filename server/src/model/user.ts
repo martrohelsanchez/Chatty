727e1667b94cd5f442848835181794ef0acc3c91
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-25 13:59:41 +0800

    🗃Add password and profile_pic field

--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -1,13 +1,15 @@
 import mongoose, {Schema, model} from 'mongoose';
 
-interface IUserSchema extends mongoose.Document {
+export interface IUserSchema extends mongoose.Document {
     username: string,
-    password: string
+    password: string,
+    profile_pic: string
 };
 
 const userSchema = new Schema({
     username: {type: String},
-    password: {type: String}
+    password: {type: String},
+    profile_pic: {type: String}
 });
 
 export default model<IUserSchema>('User', userSchema);
