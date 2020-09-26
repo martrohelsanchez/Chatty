a32c109e6966dd986bbf91f00d1e008e5cf37ac1
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-26 10:34:46 +0800

    ✨Show loading ui when fetching conversations

--- client/src/components/conversationList/ConversationList.tsx
+++ client/src/components/conversationList/ConversationList.tsx
@@ -14,15 +14,18 @@ import {UserInfo} from 'redux/actions/userInfoActions';
 const ConversationList = () => {
     const conversations = useSelector((state: rootState) => state.conversations);
     const [err, setErr] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
     const dispatch = useDispatch();
     const user = useSelector((state: rootState) => state.userInfo as UserInfo);
     const [userLastSeen, setUserLastSeen] = useState<LastSeen | null>(null);
 
     useEffect(() => {
         if (conversations.length === 0) {
+            setIsLoading(true);
             getConversationsReq(10, null, data => {
                 dispatch(retrieveConversations(data.conversations));
                 checkLastMsgDeliver(data.conversations);
+                setIsLoading(false);
             }, err => {
                 console.error(err.message)
                 setErr('Something went wrong')
@@ -44,6 +47,8 @@ const ConversationList = () => {
         for (let conversation of conversations) {
             const {last_message, _id} = conversation;
 
+            if (last_message === null) continue
+
             if (last_message.sender !== user.userId && !last_message.is_delivered) {
                 updateMsgIsDeliveredReq(_id, last_message.sender, (err) => {
                     console.error(err);
@@ -53,14 +58,14 @@ const ConversationList = () => {
         }
     }
 
-    if (conversations.length === 0) {
+    if (isLoading) {
         return <Loading />
     } else if (err) {
         return <div>{err}</div>
     }
 
     const renderConversations = conversations.map(conv => {
-        if (conv.convHasCreated) {
+        if (conv.convHasCreated && conv.last_message) {
             return <Conversation key={conv._id} conv={conv} userLastSeen={userLastSeen} />
         }
     });
