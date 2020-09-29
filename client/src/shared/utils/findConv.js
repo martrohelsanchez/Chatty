3e9544e189f15553c381d9cf0be1e9c4a323ff31
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-09-29 18:18:56 +0800

    🔥Remove file

--- client/src/shared/utils/findConv.js
+++
@@ -1,6 +0,0 @@
-function findConv(state, select) {
-    const conv = state.conversations.find(conv => conv._id === state.currConv._id);
-    return conv ? conv[select] : []
-};
-
-export default findConv;
