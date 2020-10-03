7f2fc3b675662ef398ab24b4a8ebad9ca9f19d19
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-03 10:52:45 +0800

    💄Add OuterCircle and ConvPic

--- client/src/shared/styles.ts
+++ client/src/shared/styles.ts
@@ -6,6 +6,12 @@ export const PrussianBlueBg = styled.div`
     width: 100vw;
 `;
 
+export const SherpaBlueBg = styled.div`
+    background-color: ${({theme}) => theme.dark.primary};
+    height: 100vh;
+    width: 100vw;
+`;
+
 export const ChittyMascot = styled.div`
     height: 60vw;
     max-height: 200px;
@@ -13,7 +19,7 @@ export const ChittyMascot = styled.div`
     max-width: 200px;
     margin: 0 auto 60px auto;
     border-radius: 300px;
-    background-color: ${({ theme }) => theme.dark.thirdly}
+    background-color: ${({ theme }) => theme.dark.thirdly};
 `;
 
 export const UserInfoInput = styled.input`
@@ -43,4 +49,26 @@ export const ChittyName = styled.p`
     position: absolute;
     top: 20px;
     left: 20px;
+`;
+
+export const OuterCircle = styled.div`
+    display: flex;
+    background-color: white;
+    max-height: 82px;
+    max-width: 82px;
+    height: 7vw;
+    width: 7vw;
+    border-radius: 50%;
+`;
+
+export const ConvPic = styled.div<{ pic: string | undefined }>`
+    background-color: ${({ theme }) => theme.dark.primary};
+    height: 6.5vw;
+    width: 6.5vw;
+    max-height: 76px;
+    max-width: 76px;
+    border-radius: 50%;
+    margin: auto;
+    background-image: url(${({ pic }) => pic});
+    background-size: cover;
 `;
