af70e90429c894d3e43cd70d7486934e8a211805
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-03 10:58:54 +0800

    💄Created the styles file fro UserProfile

---
+++ client/src/components/userProfile/UserProfile.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {SherpaBlueBg} from 'shared/styles';
+
+export const Bg = styled(SherpaBlueBg)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export {SherpaBlueBg}
