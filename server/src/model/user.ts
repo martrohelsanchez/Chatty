0c5dbfd8da88bfcfc239a57381cb36fb5b67d157
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-03 10:42:06 +0800

    🗃🏷️Add header and bio fields and their type

--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -3,13 +3,17 @@ import mongoose, {Schema, model} from 'mongoose';
 export interface IUserSchema extends mongoose.Document {
     username: string,
     password: string,
-    profile_pic: string
+    profile_pic: string,
+    header?: string,
+    bio?: string,
 };
 
 const userSchema = new Schema({
     username: {type: String},
     password: {type: String},
-    profile_pic: {type: String}
+    profile_pic: {type: String},
+    header: {type: String},
+    bio: {type: String},
 });
 
 export default model<IUserSchema>('User', userSchema);
