c30641920054e80aff10f3bfa46d6066fb3b4d6a
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-03 11:00:43 +0800

    💄Styled to match the signUp mockup

---
+++ client/src/components/signUp/SignUp.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import {LogIn, Register, Err} from 'components/logIn/Login.styles';
+
+import {
+    ChittyMascot, 
+    UserInfoInput as Input, 
+    NextBtn as nextBtn,
+    ChittyName
+} from 'shared/styles';
+
+export const SignUp = styled(LogIn)``;
+
+export const NextBtn = styled(nextBtn)`
+    display: inline-block;
+    margin: 60px auto 0 auto;
+`;
+
+export const BackBtn = styled(nextBtn)`
+    display: inline-block;
+    transform: rotateY(180deg);
+    margin: 60px 20px 0 auto;
+`;
+
+export const ToLogIn = styled(Register)``;
+
+export {ChittyMascot, Input, ChittyName, Err};
--- client/src/components/signUp/signUp.tsx
+++ client/src/components/signUp/signUp.tsx
@@ -1,65 +1,126 @@
-import React, { useState } from 'react';
+import React, {useState, useRef} from 'react';
 import {useHistory} from 'react-router-dom';
 import axios from 'axios';
 
-import styles from './signUp.module.css';
+import next from 'images/next.svg';
+import * as S from './SignUp.styles';
+import { getUserByUsernameReq } from 'api/APIUtils';
 
 function SignUp() {
-    const [input, setInput] = useState<string>('');
-    const [err, setErr] = useState<string | null>(null);
-    const history = useHistory();
+  const [usernameInput, setUsernameInput] = useState('');
+  const [passInput, setPassInput] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
+  const [isUsernameScreen, setIsUsernameScreen] = useState(true);
+  const confirmPassRef = useRef<HTMLInputElement>(null!);
+  const [err, setErr] = useState<string | null>(null!);
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
 
-    function signUp() {
-        axios.post('/user/signUp', {
-            username: input.trim()
+  function signUp() {
+    if (!usernameInput) {
+      return null;
+    } 
+
+    axios.post('/user/signUp', {
+        username: usernameInput.trim(),
+        password: confirmPass.trim()
+    }, {
+      withCredentials: true
+    })
+        .then(({data}) => {
+            if (!data.isUsernameTaken) {
+                setUsernameInput('')
+                history.push(`/user/${data.userId}`)
+            } else {
+                setErr('User already exists')
+            }
         })
-            .then(({data}) => {
-                if (! data.isUsernameTaken) {
-                    setInput('')
-                    history.push('/logIn')
-                } else {
-                    setErr('User already exists')
-                }
-            })
-            .catch(err => {
-                setErr('Sorry, something went wrong. Please try again later')
-            })
-        setErr(null)
-    }
+        .catch(err => {
+            setErr('Sorry, something went wrong. Please try again later')
+        })
+    setErr(null)
+  }
 
-    function onInputChange(e) {
-        setInput(e.target.value);
-    }
+  const onUsernameSubmit = () => {
+    setErr(null);
+    setIsLoading(true);
+    getUserByUsernameReq(usernameInput, (data) => {
+      if (data) {
+        //A user exist with this username
+        setErr('Username already taken');
+      } else {
+        setIsUsernameScreen(false);
+      }
+      setIsLoading(false);
+    })
+  }
 
-    function directToLogIn() {
-        history.push('/logIn')
-    }
+  const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUsernameInput(e.target.value);
+  }
 
-    return (
-      <div className={styles.signUpContainer}>
-        <h3 className={styles.promptNickname}>What's your nickname?</h3>
-        <input
-          className={styles.nicknameInput}
-          type="input"
-          value={input}
-          onChange={onInputChange}
-          onKeyDown={({ key }) => (key === 'Enter' ? signUp() : null)}
-        />
-        {<div className={styles.userTaken}>{err}</div>}
-        <button className={styles.signUpBtn} onClick={signUp}>
-          Sign Up
-        </button>
-        <br />
-        <p style={{ textAlign: "center" }}>Already have an account?</p>
-        <button
-          data-testid='logInBtn'
-          className={styles.logInBtn}
-          onClick={directToLogIn}
-        >
-          Log In
-        </button>
+  const onPassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassInput(e.target.value);
+  }
+
+  const onConfirmPassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPass(e.target.value);
+    if (passInput === e.target.value) {
+      setErr(null);
+    } else {
+      setErr('Passwords entered do not match');
+    }
+  }
+
+  const handleBack = () => {
+    setIsUsernameScreen(true);
+  }
+
+  return (
+    <S.SignUp>
+      <div>
+        <S.ChittyName>
+          Chitty
+        </S.ChittyName>
+        <S.ChittyMascot/>
+        <S.Err>{err}</S.Err>
+        {isUsernameScreen ? (
+          <S.Input
+            type="input"
+            value={usernameInput}
+            placeholder='What should everyone call you?'
+            onChange={onUsernameChange}
+            onKeyDown={({key}) => (key === 'Enter' ? onUsernameSubmit() : null)}
+          />
+        ) : (
+          <>
+            <S.Input
+              type="password"
+              value={passInput}
+              placeholder='Password'
+              onChange={onPassChange}
+              onKeyDown={({key}) => (key === 'Enter' ? confirmPassRef.current.focus() : null)}
+            />
+            <S.Input
+              type="password"
+              ref={confirmPassRef}
+              value={confirmPass}
+              placeholder='Confirm Password'
+              onChange={onConfirmPassChange}
+              onKeyDown={({key}) => (key === 'Enter' && !err && confirmPass ? signUp() : null)}
+            />
+          </>
+        )}
+        <S.ToLogIn to='/logIn'>
+          Already have an account?
+        </S.ToLogIn>
+        <div style={{textAlign: 'center'}}>
+          {isUsernameScreen || <S.BackBtn src={next} onClick={handleBack}></S.BackBtn>}
+          <S.NextBtn src={next} onClick={onUsernameSubmit}></S.NextBtn>
+        </div>
       </div>
-    );
+    </S.SignUp>
+  );
 }
 
 export default SignUp
