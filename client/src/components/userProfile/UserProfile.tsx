70324e107fe5a3f612bc1e42c07996cf131c319f
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-04 11:09:56 +0800

    🙈Hide UserProfile when user is cropping the image

--- client/src/components/userProfile/UserProfile.tsx
+++ client/src/components/userProfile/UserProfile.tsx
@@ -1,21 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import * as S from './UserProfile.styles';
 import UserInfo from 'components/userInfo/UserInfo';
 import {IsUserJustRegistered} from 'pages/appRoute/AppRoute';
+import nextIcon from 'images/next.svg';
 
 import {useSelector} from 'react-redux';
 import {rootState} from 'redux/store';
+import CropPicture from 'components/cropPicture/CropPicture';
 
 const UserProfile = () => {
+    const [isSettingHeader, setIsSettingHeader] = useState(false);
+    const [isSettingProfile, setIsSettingProfile] = useState(false);
     const user = useSelector((state: rootState) => state.userInfo);
     const isUserJustReg = useContext(IsUserJustRegistered);
 
-    console.log(user.header)
-
     return (
         <>
+        {isSettingProfile || isSettingHeader ? (
+            <CropPicture isSettingHeader={isSettingHeader} isSettingProfile={isSettingHeader} />
+        ) : (
             <S.SherpaBlueBg>
+                <S.ChittyName>
+                    Chitty
+                </S.ChittyName>
                 <div style={{height: '100px'}}></div>
                 <UserInfo 
                     convName={user.username} 
@@ -23,8 +31,12 @@ const UserProfile = () => {
                     profilePic={user.profile_pic}
                     header={user.header}
                     isSetUserScreen={true}
+                    setIsSettingHeader={setIsSettingHeader}
+                    setIsSettingProfile={setIsSettingProfile}
                 />
+                <S.NextBtn src={nextIcon} />
             </S.SherpaBlueBg>
+        )}
         </>
     )
 }
