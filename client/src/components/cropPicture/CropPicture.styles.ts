169a1ddcad7a6f62b05d74957b34dd3b471b4ebd
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-04 11:16:24 +0800

    💄Create file for styling CropPicture component

---
+++ client/src/components/cropPicture/CropPicture.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import {SherpaBlueBg, ChittyName, NextBtn} from 'shared/styles';
+
+export const Cancel = styled(NextBtn)`
+
+`;
+
+export {SherpaBlueBg, ChittyName, NextBtn};
