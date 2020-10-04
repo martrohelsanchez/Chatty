1d12dc685f5d9afa1c33c0687b5a7221523bdccf
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-04 11:17:57 +0800

    💄Match styling and feature base on mockups

--- client/src/components/infoPane/InfoPane.tsx
+++ client/src/components/infoPane/InfoPane.tsx
@@ -1,26 +1,103 @@
 import React from 'react';
-import styled from 'styled-components';
-import { useRouteMatch, Route } from 'react-router-dom';
+import {useRouteMatch, useHistory} from 'react-router-dom';
 
-const StyledInfoPane = styled.div<{showInMobile: boolean}>`
-    background-color: ${({theme}) => theme.dark.primary};
-    flex: 1.2 1 0;
+import * as S from './InfoPane.styles';
+import UserInfo from 'components/userInfo/UserInfo';
+import {getConversationName} from 'shared/utils/helpers';
+import {User} from 'shared/types/dbSchema';
 
-    @media all and (max-width: ${({theme}) => theme.mobile}) {
-        & {
-            display: ${({showInMobile}) => showInMobile ? 'block' : 'none'};
-        }
-    }
-`
+import trashCan from 'images/trash_can.svg';
+import videoCall from 'images/video_call.svg';
+import call from 'images/call.svg';
+
+import {useSelector, useDispatch} from 'react-redux';
+import {rootState} from 'redux/store';
+import { deleteConversationReq } from 'api/APIUtils';
+import { deleteConv } from 'redux/actions/conversationsActions';
 
 const InfoPane = () => {
     const infoPaneMatchRoute = useRouteMatch('/chat/:convId/info');
+    const messagesMatchRoute = useRouteMatch<{convId: string}>('/chat/:convId');
+    const currConv = useSelector((state: rootState) => state.conversations.find(conv => conv._id === messagesMatchRoute?.params.convId));
+    const user = useSelector((state: rootState) => state.userInfo);
     const showInMobile = infoPaneMatchRoute ? infoPaneMatchRoute.isExact : false;
+    const dispatch = useDispatch();
+    const history = useHistory();
+    let convName;
+    let header;
+    let profilePic;
+    let bio;
+
+    const handleDeleteConv = () => {
+        if (currConv) {
+            deleteConversationReq(currConv?._id, (data) => {
+                dispatch(deleteConv(data.deletedConv));
+                history.push('/chat');
+            });
+        }
+    }
+
+    if (currConv?.is_group_chat) {
+
+    } else {
+        if (currConv?.convHasCreated) {
+            const kausap = (currConv.members as User[]).find(member => member._id !== user.userId);
+            convName = getConversationName(currConv, user);
+            if (typeof currConv.members[0] === 'string') {
+                //members field is not populated
+                header = null;
+                profilePic = null;
+                bio = null;
+            } else {
+                header = kausap?.header;
+                profilePic = kausap?.profile_pic;
+                bio = kausap?.bio;
+            }
+        } else {
+            convName = currConv?.searchedUser.username;
+            header = currConv?.searchedUser.header;
+            profilePic = currConv?.searchedUser.profile_pic;
+        }
+    }
 
     return (
-        <StyledInfoPane showInMobile={showInMobile}>
-            hello world
-        </StyledInfoPane>
+        <S.StyledInfoPane showInMobile={showInMobile}>
+            <UserInfo 
+                isSetUserScreen={false}
+                convName={convName} 
+                bio={bio} 
+                header={header}
+                profilePic={profilePic} 
+            />
+            {currConv?.convHasCreated ? (
+                <S.ConvActionCont onClick={handleDeleteConv}>
+                    <S.convActionLabel>
+                        Delete conversation
+                    </S.convActionLabel>
+                    <S.ConvActionIcon src={trashCan} style={{height: '20px'} } />
+                </S.ConvActionCont>
+            ) : (
+                null
+            )}
+            {currConv ? (
+                <>
+                    <S.ConvActionCont>
+                        <S.convActionLabel>
+                            Video call
+                        </S.convActionLabel>
+                        <S.ConvActionIcon src={videoCall} />
+                    </S.ConvActionCont>
+                    <S.ConvActionCont>
+                        <S.convActionLabel>
+                            Call
+                        </S.convActionLabel>
+                        <S.ConvActionIcon src={call} />
+                    </S.ConvActionCont>
+                </>
+            ) : (
+                null
+            )}
+        </S.StyledInfoPane>
     )
 }
 
