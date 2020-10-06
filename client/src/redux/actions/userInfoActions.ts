1d1412471a8f03ddf8a35c6ba5b3bbbaeb6dde6b
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-06 12:08:56 +0800

    ✨Create actions for updating user's bio, header, and profile

--- client/src/redux/actions/userInfoActions.ts
+++ client/src/redux/actions/userInfoActions.ts
@@ -3,6 +3,9 @@ import {User} from 'shared/types/dbSchema';
 export type UserInfoActionTypes = 
     | ReturnType<typeof setUserInfo>
     | ReturnType<typeof resetUserInfo>
+    | ReturnType<typeof updateUserProfilePic>
+    | ReturnType<typeof updateUserHeader>
+    | ReturnType<typeof updateUserBio>
 
 export interface UserInfo extends Omit<User, '_id'> {
     userId: string;
@@ -17,6 +20,27 @@ export const setUserInfo = (userInfo: UserInfo): SetUserInfoAction => {
     return {
         type: 'userInfo/setUserInfo' as 'userInfo/setUserInfo',
         userInfo 
+    }
+}
+
+export const updateUserProfilePic = (imgUrl: string) => {
+    return {
+        type: 'userInfo/updatedProfilePic' as 'userInfo/updatedProfilePic',
+        imgUrl 
+    }
+}
+
+export const updateUserHeader = (imgUrl: string) => {
+    return {
+        type: 'userInfo/updatedHeader' as 'userInfo/updatedHeader',
+        imgUrl
+    }
+}
+
+export const updateUserBio = (bio: string) => {
+    return {
+        type: 'userInfo/updatedUserbio' as 'userInfo/updatedUserbio',
+        bio 
     }
 }
 
--- client/src/redux/reducers/userInfo.ts
+++ client/src/redux/reducers/userInfo.ts
@@ -4,6 +4,21 @@ export default (state: UserInfo = null!, action: UserInfoActionTypes) =>  {
     switch (action.type) {
         case 'userInfo/setUserInfo':
             return action.userInfo;
+        case 'userInfo/updatedProfilePic': 
+            return {
+                ...state,
+                profile_pic: action.imgUrl
+            }
+        case 'userInfo/updatedHeader': 
+            return {
+                ...state,
+                header: action.imgUrl
+            }
+        case 'userInfo/updatedUserbio': 
+            return {
+                ...state,
+                bio: action.bio
+            }
         case 'userInfo/stateReset':
             return null!;
         default:
