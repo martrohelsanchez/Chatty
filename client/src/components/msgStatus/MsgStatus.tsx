b252f1c27e8f26e69e4a4a003e9f7760d608f116
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-08 13:55:37 +0800

    💄Add styled components

--- client/src/components/msgStatus/MsgStatus.tsx
+++ client/src/components/msgStatus/MsgStatus.tsx
@@ -2,32 +2,30 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import {useRouteMatch} from 'react-router-dom';
 
-import styles from './msgStatus.module.css';
-
+import * as S from './MsgStatus.styles';
 import delivered from 'images/delivered.svg';
 import sending from 'images/sending.svg';
 import sent from 'images/sent.svg';
-import seen from 'images/seen.svg'; 
 
 import {rootState} from 'redux/store';
-import {Message, MembersMeta, PopulatedConversation} from 'shared/types/dbSchema';
+import {Message, PopulatedConversation} from 'shared/types/dbSchema';
 import {UserInfo} from 'redux/actions/userInfoActions';
 
 interface MsgStatusProps {
-    allMsg: Message[];
     msgIndex: number;
-    membersMeta: PopulatedConversation['members_meta'];
     currMsg: PopulatedConversation['messages'][0];
+    isFromUser: boolean;
+    currConv: PopulatedConversation;
 }
 
-const MsgStatus = ({allMsg, msgIndex, membersMeta, currMsg}: MsgStatusProps) => {
+const MsgStatus = ({msgIndex, currMsg, isFromUser, currConv}: MsgStatusProps) => {
     const user = useSelector((state: rootState) => state.userInfo as UserInfo);
-    const match = useRouteMatch<{convId: string}>();
-    const nextMsg = allMsg[msgIndex + 1] || {};
-    const isLastMsg = allMsg.length - 1 === msgIndex;
+    const {members_meta, messages, members} = currConv;
+    const nextMsg = messages[msgIndex + 1] || {};
+    const isLastMsg = messages.length - 1 === msgIndex;
 
     //Know how many members that this is the last message they last saw
-    let usersLastMsgSeen = membersMeta.filter(member => {
+    let usersLastMsgSeen = members_meta.filter(member => {
         const seenCurrMsg = member.last_seen >= currMsg.date_sent;
         const seenNextMsg = member.last_seen > nextMsg.date_sent;
         const userLastMsgSeen = isLastMsg ? seenCurrMsg : seenCurrMsg && !seenNextMsg;
@@ -44,11 +42,13 @@ const MsgStatus = ({allMsg, msgIndex, membersMeta, currMsg}: MsgStatusProps) => 
     //Render users who are the last seen of the message
     if (usersLastMsgSeen.length > 0) {
         return (
-            <div>
+            <S.MsgStatusContainer isFromUser={isFromUser}>
                 {usersLastMsgSeen.map(user => 
-                    <img key={user.user_id} className={styles.status} src={seen} />
+                    <S.Status key={user.user_id} src={members.find(member => (
+                        member._id === user.user_id
+                    ))?.profile_pic} />
                 )}
-            </div>
+            </S.MsgStatusContainer>
         )
     }
 
@@ -60,21 +60,21 @@ const MsgStatus = ({allMsg, msgIndex, membersMeta, currMsg}: MsgStatusProps) => 
 
         if (!isSent) {
             return (
-                <div>
-                    <img className={styles.status} src={sending} />
-                </div>
+                <S.MsgStatusContainer isFromUser={isFromUser}>
+                    <S.Status src={sending} />
+                </S.MsgStatusContainer>
             )
         } else if (isSent && !isDelivered) {
             return (
-                <div>
-                    <img className={styles.status} src={sent} />
-                </div>
+                <S.MsgStatusContainer isFromUser={isFromUser}>
+                    <S.Status src={sent} />
+                </S.MsgStatusContainer>
             )
         } else if (isDelivered) {
             return (
-                <div>
-                    <img className={styles.status} src={delivered} />
-                </div>
+                <S.MsgStatusContainer isFromUser={isFromUser}>
+                    <S.Status src={delivered} />
+                </S.MsgStatusContainer>
             )
         }
     }
