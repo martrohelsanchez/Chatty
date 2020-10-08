a2e181cd712e3885ab3dd661f6d63d83ed5ca2dc
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-08 13:51:19 +0800

    ✨Render user's username and profile picture

--- client/src/components/messagesPane/messagesPane.tsx
+++ client/src/components/messagesPane/messagesPane.tsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import {Route, useRouteMatch, useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
-import InputBar from '../inputBar/InputBar'
-import MessageList from './messages/MessageList'
+import * as S from './MessagesPane.styles';
+import InputBar from 'components/inputBar/InputBar'
+import MessageList from 'components/messages/MessageList'
+import {getConversationName, getConvPic} from 'shared/utils/helpers';
+import infoBtnIcon from 'images/info_btn.svg';
 
-import { rootState } from '../../redux/store';
-
-import {Route, useRouteMatch, useHistory, useLocation} from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-const StyledMessagePane = styled.div<{showInMobile: boolean}>`
-    background-color: ${({theme}) => theme.dark.secondary};
-    flex: 3 1 0;
-    position: relative;
-
-    @media all and (max-width: ${({theme}) => theme.mobile}) {
-        & {
-            display: ${({showInMobile}) => showInMobile ? 'block' : 'none'};
-        }
-    }
-`
+import {rootState} from 'redux/store';
+import {UserInfo} from 'redux/actions/userInfoActions';
 
 const MessagePane = () => {
     const messagesMatchRoute = useRouteMatch<{convId: string}>('/chat/:convId');
     const currConv = useSelector((state: rootState) => state.conversations.find(conv => conv._id === messagesMatchRoute?.params.convId));
     const showInMobile = messagesMatchRoute ? messagesMatchRoute.isExact : false;
+    const user = useSelector((state: rootState) => state.userInfo);
+    const history = useHistory();
+    let convPic;
+
+    if (currConv) {
+        convPic = getConvPic(currConv, user);
+    }
+
+    const onInfoBtnClick = () => {
+        history.push(`/chat/${messagesMatchRoute?.params.convId}/info`);
+    }
 
     return (
-        <StyledMessagePane showInMobile={showInMobile}>
+        <S.MessagePane showInMobile={showInMobile}>
             <Route path='/chat/:convId'>
                 {currConv ? (
                     <>
+                        <S.ConvNameCont>
+                            <S.ConvName>
+                                {getConversationName(currConv as NonNullable<typeof currConv>, user as UserInfo)}
+                            </S.ConvName>
+                            <S.InfoBtn src={infoBtnIcon} onClick={onInfoBtnClick} />
+                        </S.ConvNameCont>
+                        <S.OuterCircle>
+                            <S.ConvPic pic={convPic} />
+                        </S.OuterCircle>
                         {currConv.convHasCreated ? (
                             <MessageList currConv={currConv} />
                         ) : (
@@ -42,7 +52,7 @@ const MessagePane = () => {
                     null
                 )}
             </Route>
-        </StyledMessagePane>
+        </S.MessagePane>
     )
 }
 
