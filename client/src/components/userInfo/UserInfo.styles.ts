8d02afddb1d00d13916adc68cbcb49decae5f8eb
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-08 13:51:43 +0800

    💄Add styles

--- client/src/components/userInfo/UserInfo.styles.ts
+++ client/src/components/userInfo/UserInfo.styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import {OuterCircle as outerCircle, ConvPic as convPic} from 'shared/styles';
 
-export const Header = styled.div<{pic: string | undefined}>`
-    background-color: ${({theme}) => theme.dark.secondary};
+export const Header = styled.div<{pic: string | undefined, isSetUserScreen: boolean}>`
+    background-color: ${({theme, isSetUserScreen}) => isSetUserScreen ? theme.dark.primary : theme.dark.secondary};
     background-image: url(${({pic}) => pic});
     background-size: cover;
     width: 90%;
@@ -10,14 +10,21 @@ export const Header = styled.div<{pic: string | undefined}>`
     border-radius: 10px;
     margin: 5% auto;
     position: relative;
+
+    &:hover {
+        cursor: ${({isSetUserScreen}) => isSetUserScreen ? 'pointer' : 'default'};
+    }
 `;
 
-export const BioContainer = styled.div`
-    background-color: ${({theme}) => theme.dark.secondary};
+export const BioContainer = styled.div<{isSetUserScreen}>`
+    background-color: ${({theme, isSetUserScreen}) => isSetUserScreen ? theme.dark.primary : theme.dark.secondary};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 90%;
     margin: 0 auto 25px auto;
     border-radius: 10px;
-    padding: 30px 20px;
+    padding: 8% 20px;
     box-sizing: border-box;
 `;
 
@@ -28,9 +35,27 @@ export const ConvName = styled.p`
 `;
 
 export const Bio = styled.p`
+    display: inline-block;
     margin: 10px 0 0 0;
     font-size: .9rem;
     font-weight: lighter;
+`;
+
+export const BioInput = styled.textarea`
+    display: block;
+    background-color: transparent;
+    border: none;
+    resize: none;
+    color: white;
+    font-family: ${({theme}) => theme.font.primary};    
+    font-size: .9rem;
+    width: 100%;
+    box-sizing: border-box;
+    overflow: hidden;
+
+    &:focus {
+        outline: none;
+    }
 `;
 
 export const OuterCircle = styled(outerCircle)`
@@ -40,33 +65,19 @@ export const OuterCircle = styled(outerCircle)`
     left: 0;
     right: 0;
     box-sizing: border-box;
-
-    @media all and (max-width: ${({theme}) => theme.mobile}) {
-        & {
-            top: 82%;
-            height: 20vw;
-            width: 20vw;
-            max-height: 100px;
-            max-width: 100px;
-        }
-    }
 `; 
 
 export const ConvPic = styled(convPic)`
-    @media all and (max-width: ${({theme}) => theme.mobile}) {
-        & {
-            height: 18vw;
-            width: 18vw;
-            max-height: 90px;
-            max-width: 90px;
-        }
-    }
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: -95.5% auto 0 auto;
 `;
 
 export const UserInfoCont = styled.div`
     color: white;
     width: 100%;
-    max-width: 500px;
+    max-width: 600px;
     margin: auto;
 `;
 
@@ -76,7 +87,7 @@ export const Cross = styled.img`
     right: 0;
     top: 35%;
     margin: auto;
-    height: 25px;
+    height: 15%;
 `;
 
 export {outerCircle, convPic};
