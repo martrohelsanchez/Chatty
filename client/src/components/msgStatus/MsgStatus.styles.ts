00fe3f90a45a917a9b6cd9d2f23353bd4621ea5d
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-10-09 14:16:02 +0800

    💄Adjust spacing of msg statuses

--- client/src/components/msgStatus/MsgStatus.styles.ts
+++ client/src/components/msgStatus/MsgStatus.styles.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const Status = styled.img`
     height: 15px;
     border-radius: 10px;
-    margin: -10px 0 0 30px;
+    margin: 0 5px 0 0;
     background-color: ${({theme}) => theme.dark.primary}
 `;
 
 export const MsgStatusContainer = styled.div<{ isFromUser: boolean }>`
     align-self: ${({ isFromUser }) => isFromUser ? 'flex-end' : 'flex-start'};
+    margin: 5px 0 0 30px;
 `;
