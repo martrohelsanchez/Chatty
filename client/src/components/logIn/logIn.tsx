dd5644122fa92d3aedbef381a22f8d625e5fc2ff
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-08 11:10:44 +0800

    ✨Detect if user is typinng

--- client/src/components/logIn/logIn.tsx
+++ client/src/components/logIn/logIn.tsx
@@ -5,12 +5,15 @@ import Loading from 'components/loading/Loading';
 import {logInReq, UserAuthRes} from 'api/APIUtils';
 import * as S from './Login.styles';
 import next from 'images/next.svg'
+import ChittyTyping from 'components/chittyTyping/ChittyTyping';
 
 import {useDispatch} from 'react-redux';
 import {setUserInfo} from 'redux/actions/userInfoActions';
 
 const LogIn = () => {
     const [usernameInput, setUsernameInput] = useState('');
+    const [isTyping, setIsTyping] = useState(false);
+    const typingTimeout = useRef<number | undefined>(undefined);
     const [err, setErr] = useState<string | null>(null);
     const usernameInputRef = useRef<HTMLInputElement>(null); 
     const history = useHistory();
@@ -56,9 +59,9 @@ const LogIn = () => {
             } // if isAuth is false, server send 401 status as an Error object
         } catch (err) {
             if (!err.response) {
-                    console.error(err)
-                    return null
-                }
+                console.error(err)
+                return null
+            }
             const {status} = err.response;
             
             if (status === 401) {
@@ -71,12 +74,32 @@ const LogIn = () => {
         }
     }
 
+    function typing() {
+        clearTimeout(typingTimeout.current);
+
+        if (isTyping) {
+            setTypingTimeout();
+        } else {
+            setIsTyping(true);
+            setTypingTimeout();
+        }
+    }
+
+    function setTypingTimeout() {
+        typingTimeout.current = setTimeout(() => {
+            typingTimeout.current = undefined;
+            setIsTyping(false);
+        }, 1000);
+    }
+
     function handleUsernameChange(e) {
         setUsernameInput(e.target.value);
+        typing();
     }
 
     function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
         setPassInput(e.target.value);
+        typing();
     }
 
     return (
@@ -86,33 +109,34 @@ const LogIn = () => {
             </S.ChittyName>
             <S.Wrapper>
                 <S.ChittyMascot />
-                <S.Err>
-                    {err}
-                </S.Err>
-                <S.Input 
-                    id='nickname-input'
-                    data-testid='usernameInput'
-                    ref={usernameInputRef}
-                    autoComplete='off'
-                    placeholder='Username'
-                    type="text"
-                    value={usernameInput}
-                    autoFocus
-                    onChange={handleUsernameChange}
-                    onKeyDown={({key}) => key === 'Enter' ? passInputRef.current.focus() : null}
-                />
-                <S.Input
-                    type='password'
-                    ref={passInputRef}
-                    value={passInput}
-                    autoComplete="off"
-                    onChange={handlePasswordChange} 
-                    placeholder='Password'
-                    onKeyDown={({key}) => key === 'Enter' ? handleLogIn() : null}
-                />
-                <S.Register to='/signUp'>
-                    Register
-                </S.Register>
+                    <S.Err>
+                        {err}
+                    </S.Err>
+                    <S.Input 
+                        id='nickname-input'
+                        data-testid='usernameInput'
+                        ref={usernameInputRef}
+                        autoComplete='off'
+                        placeholder='Username'
+                        type="text"
+                        value={usernameInput}
+                        autoFocus
+                        onChange={handleUsernameChange}
+                        onKeyDown={({key}) => key === 'Enter' ? passInputRef.current.focus() : null}
+                    />
+                    <S.Input
+                        type='password'
+                        ref={passInputRef}
+                        value={passInput}
+                        autoComplete="off"
+                        onChange={handlePasswordChange} 
+                        placeholder='Password'
+                        onKeyDown={(e) => e.key === 'Enter' ? handleLogIn() : null}
+                    />
+                    <S.Register to='/signUp'>
+                        Register
+                    </S.Register>
+                </div>
                 <div style={{textAlign: 'center'}}>
                     {isLoading ? (
                         <Loading style={{ display: 'inline-block', margin: '60px auto 0 auto'}} />
