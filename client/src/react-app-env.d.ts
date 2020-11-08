a7085eb3f4867e9d9d31f58eff9fe72a23275f84
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-11-08 10:34:24 +0800

    🏷️Add typing for dotenv

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+export interface IProcessEnv {
+    REACT_APP_SERVER_URL: string;
+}
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv extends IProcessEnv { }
+    }
+}
